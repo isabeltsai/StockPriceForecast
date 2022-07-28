b8b806715ba78ba899f288530ce96e59a9a00f41
isabeltsai <110067354+isabeltsai@users.noreply.github.com>
2022-07-28 15:50:37 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-  name="StockPriceForecast"
-  author= "Isabel Tsai"
-  author_email= "isabeltsai0905@gmail.com"
-  license= "MIT license"
-  description= "Predicts prices of stock markets' ending price"
-  packages= ["forecast"]
+  name ="StockPriceForecast"
+  author = "Isabel Tsai"
+  author_email = "isabeltsai0905@gmail.com"
+  license = "MIT license"
+  description = "Predicts prices of stock markets' ending price"
+  packages = ["forecast"]
 )
